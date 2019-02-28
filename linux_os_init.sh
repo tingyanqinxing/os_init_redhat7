e3983f60ba37893e0ed75c6976db74600e3fdfdf
flint <qqc.flint@gmail.com>
2019-02-28 15:51:29 +0800

    add backup_db_web.sh

--- linux_os_init.sh
+++ linux_os_init.sh
@@ -1,8 +1,11 @@
-#$/bin/bash
-
+#!/bin/bash
+##current dir
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 ###常用软件
 commonSoft=('vim' 'net-tools' 'iptables-services' 'wget' 'ntpdate')
 zabbixServer="129.204.93.132,223.119.51.10"
+ngx_install_prefix="/usr/local/nginx"
+php_install_prefix="/usr/local/php7"
 
 installCommonSoft(){
 	for i in ${commonSoft[@]}
@@ -136,7 +139,7 @@ osBaseConfig(){
 ####install zabbix agent
 installZabbixAgent(){
 	zabbixRepo="http://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm"
-	
+	###判断是否已经运行zabbix
 	ps aux | grep -i zabbix | grep -v grep > /dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
@@ -144,10 +147,11 @@ installZabbixAgent(){
 		yum install -y zabbix-agent
 		sed -i "s/Server=127.0.0.1/Server=${zabbixServer}/" /etc/zabbix/zabbix_agentd.conf
 		sed -i "s/ServerActive=127.0.0.1/ServerActive=${zabbixServer}/" /etc/zabbix/zabbix_agentd.conf   
-		echo "Include=/etc/zabbix/zabbix_agentd.conf.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
+		echo "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
 		
-		mkdir /etc/zabbix/zabbix_agentd.conf.d
-		cp zabbix/* /etc/zabbix/zabbix_agentd.conf.d/
+		mkdir /etc/zabbix/zabbix_agentd.d
+		cp zabbix/* /etc/zabbix/zabbix_agentd.d/
+		
 		systemctl restart zabbix-agent.service
 		systemctl enable zabbix-agent.service
 
@@ -165,6 +169,102 @@ installZabbixAgent(){
 	
 }
 
+installNginx1_14_2(){
+	###判断是否已经运行nginx，如果已经有实例运行，不安装
+	ps aux | grep -i "nginx: master process"  | grep -v grep > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+		##download software
+		wget http://nginx.org/download/nginx-1.14.2.tar.gz
+		wget "http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz"
+	
+		##add group and user
+		groupadd www
+		useradd -s /sbin/nologin -M -g www www
+		##install dependency
+		yum install -y GeoIP-devel gcc pcre-dev openssl-devel 
+		
+		tar -xf nginx-1.14.2.tar.gz
+		tar -xf ngx_cache_purge-2.3.tar.gz
+		
+		cd nginx-1.14.2
+		./nginx-1.14.2/configure --user=www --group=www --prefix=${ngx_install_prefix} --with-http_v2_module --with-http_ssl_module --with-http_sub_module --with-http_flv_module --with-http_stub_status_module --with-http_gzip_static_module --with-pcre  --with-http_realip_module  --with-http_geoip_module  --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --add-module=ngx_cache_purge-2.3
+		
+		make &&  make install
+		cd ..
+		
+		
+		cat>/lib/systemd/system/nginx.service<<EOF
+[Unit]
+	Description=nginx
+	After=network.target
+
+	[Service]
+	Type=forking
+	ExecStart=${ngx_install_prefix}/sbin/nginx
+	ExecReload=${ngx_install_prefix}/sbin/nginx -s reload
+	ExecStop=${ngx_install_prefix}/sbin/nginx -s quit
+	PrivateTmp=true
+
+	[Install]
+	WantedBy=multi-user.target
+
+EOF
+		systemctl enable nginx
+	else
+		echo "nginx already installed"
+	fi
+}
+
+installPHP7_2_0(){
+	###判断是否已经运行PHP，如果已经有实例运行，不安装
+	ps aux | grep -i "php-fpm: master process"  | grep -v grep > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+		yum install -y gcc libxml2-devel libjpeg-turbo-devel libpng-devel libxslt libxslt-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel curl curl-devel openssl openssl-devel  bzip2  bzip2-devel  libxslt libxslt-devel  libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel curl curl-devel openssl openssl-devel perl perl-devel httpd-devel
+		wget http://am1.php.net/distributions/php-7.2.0.tar.bz2
+		tar -xf php-7.2.0.tar.bz2
+		cd php-7.2.0
+		./configure --prefix=${php_install_prefix} --with-curl --with-freetype-dir --with-gd --with-gettext --with-iconv-dir --with-kerberos --with-libdir=lib64 --with-libxml-dir --with-mysqli --with-openssl --with-pcre-regex --with-pdo-mysql --with-pdo-sqlite --with-pear --with-png-dir --with-xmlrpc --with-xsl --with-zlib --enable-fpm --enable-bcmath -enable-inline-optimization --enable-gd-native-ttf --enable-mbregex --enable-mbstring --enable-opcache --enable-pcntl --enable-shmop --enable-soap --enable-sockets --enable-sysvsem --enable-xml --enable-zip --enable-pcntl --with-curl --with-fpm-user=www --enable-ftp --enable-session --enable-xml --with-apxs2=/bin/apxs
+		
+		make && make install
+		cd ..
+		
+		
+		cp php-7.2.0/php.ini-production ${php_install_prefix}/etc/php.ini
+		
+		mv ${php_install_prefix}/etc/php-fpm.conf.default ${php_install_prefix}/etc/php-fpm.conf
+		mv ${php_install_prefix}/etc/php-fpm.d/www.conf.default ${php_install_prefix}/etc/php-fpm.d/www.conf
+		\cp -f php-7.2.0/sapi/fpm/php-fpm.service /usr/lib/systemd/system/php-fpm.service
+		
+		systemctl enable php-fpm
+		systemctl start php-fpm
+		
+	else
+		echo "PHP already installed"
+	fi
+}
+
+installMysql5_7(){
+	###判断是否已经运行PHP，如果已经有实例运行，不安装
+	ps aux | grep -i "mysqld"  | grep -v grep > /dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+		rpm -ivh https://repo.mysql.com//mysql80-community-release-el7-2.noarch.rpm
+		yum install -y yum-utils
+		yum-config-manager --disable mysql80-community
+		yum-config-manager --enable mysql57-community
+		yum install -y mysql-community-server
+		
+		systemctl start mysqld.service
+		systemctl enable mysqld.service
+		
+	else
+	
+	fi
+
+}
+
 main(){
 	echo "check OS version..."
 	checkOSVersion=$(getOSVersion)
@@ -172,10 +272,24 @@ main(){
 	if [ "${checkOSVersion}" == "RHEL7" ]
 	then
 		echo "os version OK"
+		read -p""
+		read -p"Install nginx1.14.2 or not[y|n]: " -n 1 choice_install_nginx
+		read -p"Install php7.2.0 or not[y|n]: " -n 1 choice_install_php
+		
+		
 		installCommonSoft
 		echo "start OS basic config..."
 		osBaseConfig
 		installZabbixAgent
+		
+		
+		case "${choice}" in 
+			y) installNginx1_14_2;;
+			n) echo "skip installing nginx...";;
+			*) echo "skip installing nginx...";;
+		esac
+		
+		
 
 	fi
 }
